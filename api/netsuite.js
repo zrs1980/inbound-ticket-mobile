6770ecf9b077982ef78c7ffe4dac9903080918e7
Zabe Siddique <zabe@cebasolutions.onmicrosoft.com>
2026-03-19 16:23:38 -0700

    Disable response caching to fix intermittent 401s

--- api/netsuite.js
+++ api/netsuite.js
@@ -58,6 +58,8 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
 
   if (req.method === 'OPTIONS') return res.status(200).end();
 
